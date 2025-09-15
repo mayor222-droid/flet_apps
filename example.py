f5618d0d85fbe988773894a493cdb9c54bd63e6d
UbongPr7 <ubongpr7@gmail.com>
2025-09-15 23:07:31 +0100

    added main.py

---
+++ example.py
@@ -0,0 +1,10 @@
+from typing import Any
+
+dict_={
+    'string':'bb'
+}
+
+def greeting(name:str,age:int,favorite_colors:list[str],choices:dict[str,Any]):
+    # name.
+    
+    print(f'Hello {name}')
